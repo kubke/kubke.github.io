349b258b72b8f10b6c8dd949d9adaeb0bfebab13
kubke <mf.kubke@gmail.com>
2017-11-24 16:38:10 +1300

    once more

--- assets/js/obfuscate-email.js
+++ assets/js/obfuscate-email.js
@@ -3,6 +3,6 @@
 // This code is freeware provided these four comment lines remain intact
 // A wizard to generate this code is at http://www.jottings.com/obfuscator/
 { 
-  document.write("<a href='mailto:"mf.kubke@gmail.com"</a>")
+  document.write("<a href='mailto:"mf.kubke@gmail.com"'</a>")
 }
 

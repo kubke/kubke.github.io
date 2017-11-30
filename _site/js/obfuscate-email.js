8cdf6ef0e6787f9e1af6d891f3191e3a717dcab1
kubke <mf.kubke@gmail.com>
2017-11-30 14:05:51 +1300

    Third time's a charm

--- _site/js/obfuscate-email.js
+++ _site/js/obfuscate-email.js
@@ -2,8 +2,10 @@
 // Random encryption key feature by Andrew Moulden, Site Engineering Ltd
 // This code is freeware provided these four comment lines remain intact
 // A wizard to generate this code is at http://www.jottings.com/obfuscator/
-{ coded = "jLN4ggvS@SfOv7.jNf"
-  key = "NC5aSsI1UwMHypjluEi0DOW6xkF3P8tZ97mevbGrQAXTozJhgBVYf4KLRncd2q"
+
+
+{ coded = "xM.hk2h6@5xrcB.Vpx"
+  key = "gtz6iBo1aGAkqXwydZ5sFUcCpLrNOhTE9fHYPvR0V7mb3KWe4lIMnQjuSDJ8x2"
   shift=coded.length
   link=""
   for (i=0; i<coded.length; i++) {
@@ -16,6 +18,6 @@
       link += (key.charAt(ltr))
     }
   }
-  document.write("<a href='mailto:"+link+"'>"+link+"</a>")
+document.write("<a href='mailto:"+link+"'>Mail me</a>")
 }
 
